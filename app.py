860ef22d33e1fcfcde466093ee87a8f8855a7bf2
tayyab-balti <tayyabkazmi555@gmail.com>
2024-09-13 09:37:45 +0500

    First Push

--- app.py
+++ app.py
@@ -23,34 +23,6 @@ y = df.iloc[:, -1]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# # Function to perform feature engineering
-# def feature_engineering(df, X, y, k=5):
-#     # Feature selection with SelectKBest
-#     chi2_selector = SelectKBest(chi2, k=k)
-#     X_new = chi2_selector.fit_transform(X, y)
-
-#     # Get selected features
-#     selected_features_indices = chi2_selector.get_support(indices=True)
-#     selected_features = X.columns[selected_features_indices]
-
-#     # Display selected features in Streamlit
-#     st.write("Selected feature indices:", selected_features_indices)
-#     st.write("Selected features:", selected_features.tolist())
-
-#     # Create DataFrame with selected features
-#     selected_features_df = df[selected_features].copy()
-#     selected_features_df['Outcome'] = y
-
-#     # Display DataFrame in Streamlit
-#     st.write("DataFrame with selected features:", selected_features_df)
-
-#     # Split the data
-#     x1 = selected_features_df.drop(["Outcome"], axis=1)
-#     y1 = selected_features_df["Outcome"]
-#     X_train, X_test, y_train, y_test = train_test_split(x1, y1, test_size=0.2, random_state=42)
-
-#     return X_train, X_test, y_train, y_test, selected_features
-
 def user_report():
     pregnancies = st.sidebar.slider('Pregnancies', 0, 17, 3)    # min, max, starting_value
     glucose = st.sidebar.slider('Glucose', 0, 199, 120)
